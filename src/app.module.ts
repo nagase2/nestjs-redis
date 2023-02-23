dbff809fc9375aef5b6480d9a29147a17d919fb2
Nagase Yoshimasa <yoshimasa_nagase@tsyscom.co.jp>
2023-02-24 00:42:29 +0900

    add pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
+  imports: [CacheModule.register({ isGlobal: true }), PokemonModule],
   controllers: [AppController],
   providers: [AppService],
 })
